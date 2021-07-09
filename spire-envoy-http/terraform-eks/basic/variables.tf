92b3893ca4d5032075426e4eacb4e1b6cc23d33e
Prasad Adireddi <prasad.adireddi@hpe.com>
2021-07-09 08:39:11 +0000

    Spire + Envoy usecase for http workloads

---
+++ spire-envoy-http/terraform-eks/basic/variables.tf
@@ -0,0 +1,47 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "map_accounts" {
+  description = "Additional AWS account numbers to add to the aws-auth configmap."
+  type        = list(string)
+
+  default = [
+    "777777777777",
+    "888888888888",
+  ]
+}
+
+variable "map_roles" {
+  description = "Additional IAM roles to add to the aws-auth configmap."
+  type = list(object({
+    rolearn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      rolearn  = "arn:aws:iam::xxxxxxxxx:user/prasad"
+      username = "prasad"
+      groups   = ["system:masters"]
+    },
+  ]
+}
+
+variable "map_users" {
+  description = "Additional IAM users to add to the aws-auth configmap."
+  type = list(object({
+    userarn  = string
+    username = string
+    groups   = list(string)
+  }))
+
+  default = [
+    {
+      userarn  = "arn:aws:iam::xxxxxxxxx:user/prasad"
+      username = "prasad"
+      groups   = ["system:masters"]
+    }
+  ]
+}
