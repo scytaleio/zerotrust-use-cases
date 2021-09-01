6779b228ac11756b7d5bc959ed70ef6d12dd9615
Prasad Adireddi <prasad.adireddi@hpe.com>
2021-09-01 16:29:32 +0000

    Fixed module names in main.tf

--- spire-vault-config/main.tf
+++ spire-vault-config/main.tf
@@ -25,7 +25,7 @@ provider "kubernetes" {
   }
 }
 
-module "acm-certificate7" {
+module "acm-certificate" {
   source = "../spire-eks/acm-certificate"
   region = "${module.aws-eks.region}"
   trust_domain = "scytale-oidc-vault-tf.spire-test.com"
@@ -33,43 +33,43 @@ module "acm-certificate7" {
   depends_on = [module.aws-eks]
 }
 
-module "spire-server-oidc7" {
+module "spire-server-oidc" {
   source = "../spire-eks/spire-server-oidc"
-  acm_certificate_arn = "${module.acm-certificate7.acm_certificate_arn}"
+  acm_certificate_arn = "${module.acm-certificate.acm_certificate_arn}"
   trust_domain = "scytale-oidc-vault-tf.spire-test.com"
   kubeconfig = "${module.aws-eks.kubeconfig_path}/${module.aws-eks.eks_cluster_name}"
-  depends_on = [module.acm-certificate7]
+  depends_on = [module.acm-certificate]
 }
 
-module "spire-agent7" {
+module "spire-agent" {
   source = "../spire-eks/spire-agent"
   trust_domain = "scytale-oidc-vault-tf.spire-test.com"
   kubeconfig = "${module.aws-eks.kubeconfig_path}/${module.aws-eks.eks_cluster_name}"
-  depends_on = [module.spire-server-oidc7]
+  depends_on = [module.spire-server-oidc]
 }
 
-module "trust-domain7" {
+module "trust-domain" {
   source = "../spire-eks/trust-domain"
-  acm_certificate_arn = "${module.acm-certificate7.acm_certificate_arn}"
+  acm_certificate_arn = "${module.acm-certificate.acm_certificate_arn}"
   region = "${module.aws-eks.region}"
-  elb = "${module.spire-server-oidc7.spire_oidc_lb}"
-  elbname = "${module.spire-server-oidc7.spire_oidc_lbname}"
+  elb = "${module.spire-server-oidc.spire_oidc_lb}"
+  elbname = "${module.spire-server-oidc.spire_oidc_lbname}"
   trust_domain = "scytale-oidc-vault-tf.spire-test.com"
   kubeconfig = "${module.aws-eks.kubeconfig_path}/${module.aws-eks.eks_cluster_name}"
-  depends_on = [module.spire-server-oidc7]
+  depends_on = [module.spire-server-oidc]
 }
 
-module "vault-setup7" {
+module "vault-setup" {
   source = "./modules/vault-setup"
   trust_domain = "scytale-oidc-vault-tf.spire-test.com"
   kubeconfig = "${module.aws-eks.kubeconfig_path}/${module.aws-eks.eks_cluster_name}"
-  depends_on = [module.spire-agent7]
+  depends_on = [module.spire-agent]
 }
 
-module "vault-configure7" {
+module "vault-configure" {
   source = "./modules/vault-configure"
   trust_domain = "scytale-oidc-vault-tf.spire-test.com"
   kubeconfig = "${module.aws-eks.kubeconfig_path}/${module.aws-eks.eks_cluster_name}"
-  depends_on = [module.trust-domain7,module.vault-setup7]
+  depends_on = [module.trust-domain,module.vault-setup]
 }
 
